4e38d5a9cfbf89fca31b577cc557c4fc77097303
CaioDB1 <caiodanielfbueno@gmail.com>
2024-05-06 17:13:27 -0300

    for

---
+++ JavaScript/aula-04.js
@@ -0,0 +1,9 @@
+const quantidade = 10;
+let soma = 1;
+
+for(let i = 0; i < quantidade; i++){
+    console.log(i)
+    console.log('Numero')
+    soma = soma + i
+}
+console.log('Resultado = ', soma);
