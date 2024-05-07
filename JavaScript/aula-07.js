4ca033bb99ce81a4484e18100fadd0bdaad0989b
CaioDB1 <caiodanielfbueno@gmail.com>
2024-05-07 15:35:05 -0300

    array

---
+++ JavaScript/aula-07.js
@@ -0,0 +1,8 @@
+const idades = [18, 14, 'aaaaaaa', 53, 2, 80, 15, 49, 100]
+
+console.log('Resultado', idades[1])
+console.log('Resultado', idades.length)
+
+for(let i = 0; i < idades.length; i++){
+    console.log('Resultado', idades[i])
+}
