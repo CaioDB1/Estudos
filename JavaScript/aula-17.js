b319b943fffc3a0d629837c5f4dd14e81d4fcdc5
CaioDB1 <caiodanielfbueno@gmail.com>
2024-05-08 19:01:15 -0300

    Switch case

---
+++ JavaScript/aula-17.js
@@ -0,0 +1,25 @@
+
+
+
+function waifus(waifu){
+    switch (waifu) {
+        case 'Megumin':
+        case 'Ovo de pascoa':
+            console.log('Megumin é a melhor')
+            break;
+        case 'Nagatoro':
+            console.log('Nagatoro é a melhor')
+            break;
+        case 'Rem':
+            console.log('Rem é a melhor')
+            break;
+        default:
+            console.log('Todas são incriveis')
+            break;
+    }
+}
+
+waifus('Megumin')
+waifus('Nagatoro')
+waifus('Rem')
+waifus('aaaaaaaa')
