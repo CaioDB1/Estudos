438fa6b2ca74efd8b9a68e7b4e8ec319911c6869
CaioDB1 <caiodanielfbueno@gmail.com>
2024-05-08 18:46:47 -0300

    Variaveis primitivas e imutaveis

---
+++ JavaScript/aula-16.js
@@ -0,0 +1,11 @@
+const numero = 54949;
+const stringA = 'eigfioie';
+const vddOuFalso = true; // false
+
+stringA[0] = 'aaa'
+
+const arrayTest = [4255, 516, 16]
+arrayTest.push(10000);
+
+console.log(arrayTest) 
+console.log(stringA)
