f139cc21c0305a6dda646d155419b1cd3e56b5c0
CaioDB1 <caiodanielfbueno@gmail.com>
2024-05-21 14:48:07 -0300

    .reduce

---
+++ JavaScript/aula-30.js
@@ -0,0 +1,39 @@
+const personagens = [
+    { nome: "Chika", idade: 17, altura: 157 }, // Kaguya-sama: Love is War
+    { nome: "Tohru", idade: 18, altura: 160 }, // Fruits Basket
+    { nome: "Nadeshiko", idade: 16, altura: 157 }, // Yuru Camp△
+    { nome: "Komi", idade: 16, altura: 168 }, // Komi Can't Communicate
+    { nome: "Marin", idade: 16, altura: 165 }, // My Dress-Up Darling
+    { nome: "Anya", idade: 6, altura: 95 }, // Spy x Family
+    { nome: "Ochaco", idade: 16, altura: 156 }, // My Hero Academia
+    { nome: "Taiga", idade: 17, altura: 145 }, // Toradora!
+    { nome: "Hinata", idade: 16, altura: 160 }, // Naruto
+    { nome: "Nezuko", idade: 14, altura: 153 }, // Demon Slayer
+  ];
+
+const personagem = {
+    chika: {
+        idade: 17,
+    },
+    tohru: {
+        idade: 18,
+    },
+}
+console.log(personagem.chika.idade);
+
+
+
+
+
+
+const pessoas = personagens.reduce((acc, objeto) => {
+    return {
+        ...acc,
+        [objeto.nome]: {
+            idade: objeto.idade
+        }
+    }
+}, {})
+
+console.log(pessoas);
+console.log(pessoas.Chika.idade)
